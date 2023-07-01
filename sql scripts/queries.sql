abf758e2e8aa04f0a7d52a2705d0fe0005e8270a
Giuseppe Piccolo <giuseppe.piccolo@protonmail.com>
2023-07-01 11:26:17 +0200

    removed diplicated code

--- sql scripts/queries.sql
+++ sql scripts/queries.sql
@@ -179,7 +179,7 @@ where V.id_anno = A.id_anno and V.matricola = $matricola and datepart('YY', curr
 
 -- stipendi annuali per dipendente (2017-2022)
 create view stipendio_anno_17_22v as
-select D.anno as "Anno", B.matricola as "Matricola", sum(B.stipendio_lordo) as "Stipendio"
+select D.anno as "Anno", B.matricola as "Matricola", sum(B.stipendio_mensile) as "Stipendio"
 from fact_busta_paga  as  B, dim_data as  D
 where B.id_data = D.id_data and (datepart('YY', current_date) - D.anno) <= 6 and datepart('YY', current_date) > D.anno
 group by D.anno, B.matricola;
@@ -189,13 +189,13 @@ select v1.matricola, v2.stipendio - v1.stipendio as delta, v1.anno, v2.anno
 from stipendio_anno_17_22v as v1, stipendio_anno_17_22v as v2
 where v1.matricola = v2.matricola and v1.anno = 2017 and v2.anno = 2018;
 
-select v1.matricola, max(v2.stipendio - v1.stipendio) as max_aumento 
+select v1.matricola, max(v2.stipendio - v1.stipendio) as max_aumento
 from stipendio_anno_17_22v as v1, stipendio_anno_17_22v as v2
 where v1.matricola = v2.matricola group by v1.matricola;
 
 --stipendi annuali per dipendente (2005-2022)
 create view stipendio_anno_v as
-select D.anno as "Anno", B.matricola as "Matricola", sum(B.stipendio_lordo) as "Stipendio"
+select D.anno as "Anno", B.matricola as "Matricola", sum(B.stipendio_mensile) as "Stipendio"
 from fact_busta_paga  as  B, dim_data as  D
 where B.id_data = D.id_data and datepart('YY', current_date) > D.anno
 group by D.anno, B.matricola;
@@ -208,21 +208,7 @@ where v1.matricola = v2.matricola;
 
 delete from delta_stipendi where anno1 <> anno2 - 1;
 
--- stipendi annuali per dipendente (2005-2022)
-create view stipendio_anno_v as
-select D.anno as "Anno", B.matricola as "Matricola", sum(B.stipendio_lordo) as "Stipendio"
-from fact_busta_paga  as  B, dim_data as  D
-where B.id_data = D.id_data and datepart('YY', current_date) > D.anno
-group by D.anno, B.matricola;
-
--- delta stipendi annuali
-create view delta_stipendi as
-select v1.anno as  Anno1, v2.anno as  Anno2, v2.stipendio - v1.stipendio as  delta, v1.matricola
-from stipendio_anno_v  as  v1, stipendio_anno_v as  v2
-where v1.matricola = v2.matricola;
-
-delete from delta_stipendi where anno1 <> anno2 - 1;
-
+select * from delta_stipendi
 
 -- Situazioni particolari
 -- Top N stipendi (con info sull'impiegato)
